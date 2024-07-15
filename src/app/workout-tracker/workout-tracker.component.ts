19b13e6108d9677bc60d6df49788383af04b5085
pratikg1234 <gpratik246@gmail.com>
2024-07-15 17:32:44 +0700

    created the new angular application and added workout-tracker component

---
+++ src/app/workout-tracker/workout-tracker.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-workout-tracker',
+  standalone: true,
+  imports: [],
+  templateUrl: './workout-tracker.component.html',
+  styleUrl: './workout-tracker.component.css'
+})
+export class WorkoutTrackerComponent {
+
+}
